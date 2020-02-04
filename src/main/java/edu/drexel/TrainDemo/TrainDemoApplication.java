ee00981edca8866d922be56025155a3203e6876a
Jason Lefever <jtl86@drexel.edu>
2020-02-03 19:08:22 -0500

    Add Google and Github OAuth2 sign in

--- src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
+++ src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
@@ -7,14 +7,51 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class TrainDemoApplication {
+@RestController
+public class TrainDemoApplication extends WebSecurityConfigurerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(TrainDemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TrainDemoApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .oauth2Login();
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
     @Bean
