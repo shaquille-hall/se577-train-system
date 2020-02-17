95d933597e03b6bda37d386aa5ebfb3227a6840f
Saffat Hasan <saffathasan@gmail.com>
2020-02-17 17:40:47 -0500

    refactor: move UserController to user component

--- src/main/java/edu/drexel/TrainDemo/user/controllers/UserController.java
+++ src/main/java/edu/drexel/TrainDemo/user/controllers/UserController.java
@@ -1,4 +1,4 @@
-package edu.drexel.TrainDemo.controllers;
+package edu.drexel.TrainDemo.user.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
