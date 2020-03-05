8d431df44de68029851465449eeec2def10998d6
Saffat Hasan <saffathasan@gmail.com>
2020-03-05 18:54:48 -0500

    feat: add user/manage

--- src/main/java/edu/drexel/TrainDemo/user/controllers/UserRestController.java
+++ src/main/java/edu/drexel/TrainDemo/user/controllers/UserRestController.java
@@ -5,24 +5,16 @@ import edu.drexel.TrainDemo.user.repositories.UserRepository;
 import edu.drexel.TrainDemo.user.services.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class UserController {
+@RestController
+public class UserRestController {
 
     private UserService service;
 
-    public UserController(UserRepository repo) {
+    public UserRestController(UserRepository repo) {
         this.service = new UserService(repo);
-    }
-
-    @RequestMapping("/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", "foo");
-        return "greeting";
     }
 
     @GetMapping("/user")
@@ -30,29 +22,6 @@ public class UserController {
         Integer id = principal.getAttribute("id");
         String defaultName = principal.getAttribute("name");
         return service.getOrCreateUser(id, defaultName);
-    }
-
-    @GetMapping("/user/oauth")
-    public Object getUserOathInfo(@AuthenticationPrincipal OAuth2User principal) {
-        return principal.getAttributes();
-    }
-
-    @GetMapping("/user/manage")
-    public String manageUser(@AuthenticationPrincipal OAuth2User principal, Model model) {
-        // 1. Get the from the database
-        // getUserByExternalId
-        // if not exist, create
-        // CreateUserWithExternalId
-        // 2. Display their information on the page
-        String name = getUserInfo(principal).getName();
-        model.addAttribute("name", name);
-        return "manage_user.html";
-    }
-
-    // DEBUG ENDPOINT
-    @GetMapping("/user/test")
-    public Object testUser(@AuthenticationPrincipal OAuth2User principal) {
-        return null;
     }
 
 }
---
+++ src/main/java/edu/drexel/TrainDemo/user/controllers/UserViewController.java
@@ -0,0 +1,25 @@
+package edu.drexel.TrainDemo.user.controllers;
+
+import edu.drexel.TrainDemo.user.models.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+
+    private UserRestController user;
+
+    public UserViewController(UserRestController userRestController) {
+        this.user = userRestController;
+    }
+
+    @GetMapping("/user/manage")
+    public String manageUser(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        User current = user.getUserInfo(principal);
+        model.addAttribute("user", current);
+        return "user/manage_user";
+    }
+}
