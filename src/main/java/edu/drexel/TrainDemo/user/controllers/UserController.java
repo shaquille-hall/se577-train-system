fb113cd4864341ed3eda86fa0d5d7ce20f9f5c24
Saffat Hasan <saffathasan@gmail.com>
2020-03-06 23:28:23 -0500

    fix: merge UserRestController and UserViewController

--- src/main/java/edu/drexel/TrainDemo/user/controllers/UserController.java
+++ src/main/java/edu/drexel/TrainDemo/user/controllers/UserController.java
@@ -5,21 +5,24 @@ import edu.drexel.TrainDemo.user.repositories.UserRepository;
 import edu.drexel.TrainDemo.user.services.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-public class UserRestController {
+@Controller
+public class UserController {
 
     private UserService service;
 
-    public UserRestController(UserRepository repo) {
+    public UserController(UserRepository repo) {
         this.service = new UserService(repo);
     }
 
     @GetMapping("/user")
+    @ResponseBody
     public User getUserInfo(@AuthenticationPrincipal OAuth2User principal) {
         Integer id = principal.getAttribute("id");
         String defaultName = principal.getAttribute("name");
@@ -27,9 +30,17 @@ public class UserRestController {
     }
 
     @PostMapping("/user/manage/submit")
+    @ResponseBody
     public Object submitUserInfo(@AuthenticationPrincipal OAuth2User principal, @ModelAttribute User newUserData) {
         User originalUserData = getUserInfo(principal);
         service.saveUser(originalUserData, newUserData);
         return "<script>window.location.href = '/';</script>";
+    }
+
+    @GetMapping("/user/manage")
+    public String manageUser(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        User current = getUserInfo(principal);
+        model.addAttribute("User", current);
+        return "user/manage_user";
     }
 }
